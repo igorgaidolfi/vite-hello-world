3b72d5421fce3862a71c37ab2c1836a20ba6210e
igorgaidolfi <gandalf.4@outlook.it>
2023-12-21 16:46:40 +0100

    update Created AppHero Bonus Completato

---
+++ src/components/AppHero.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+
+}
+</script>
+<template lang="">
+    <div>
+        <img src="https://assets3.thrillist.com/v1/image/3127554/1584x1056/crop;webp=auto;jpeg_quality=60;progressive.jpg" alt="jumbotron-sofi">
+    </div>
+</template>
+<style lang="">
+    
+</style>
